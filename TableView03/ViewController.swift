94d44bc0eb2e216ca8eaa310c167c172bf43d039
dit03 <dit@imac-4.local>
2019-04-30 15:16:59 +0900

    TestTableView

--- TableView03/ViewController.swift
+++ TableView03/ViewController.swift
@@ -1,3 +1,4 @@
+
 //
 //  ViewController.swift
 //  TableView03
@@ -8,11 +9,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var myTableView: UITableView!
+    
+    var animals = ["Cat", "Dog", "Pig", "Bird", "Whale", "Cow"]
+    var detail = ["고양이", "강아지", "돼지", "새", "고래", "소"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return animals.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = myTableView.dequeueReusableCell(withIdentifier: "ID", for: indexPath)//셀 하나 만드는데 재활용
+        let row = indexPath.row
+        cell.textLabel?.text = animals[row]
+        cell.detailTextLabel?.text = detail[row]
+        
+        //이미지 넣기
+        cell.imageView?.image = UIImage(named: animals[row])
+        
+        return cell
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myTableView.dataSource = self
     }
 
 
